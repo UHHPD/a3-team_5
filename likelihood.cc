75cb8274ce44458565d5f79aeaf6baac21d6d8b4
moritzhw <>
2021-05-03 15:31:50 +0000

    versuch2

--- likelihood.cc
+++ likelihood.cc
@@ -4,17 +4,30 @@
 
 
 double poisson(double mu, int k) {
-    return 0;
+    double zaehl= pow(mu,k)*exp(-mu);
+    double nenn=tgamma(k+1);
+    return zaehl/nenn;
+}
+
+double prob(std::vector<int> daten, double mu) {
+  double lik=1;
+  for(int k : daten) {
+    poisson
+  }
+  return 0;
 }
 
 int main() {
     using namespace std;
 
+    const int N = 234;
 
+    vector<int> daten;
     ifstream fin("datensumme.txt");
     int n_i;
     for(int i = 0 ; i < 234 ; ++i) {
         fin >> n_i;
+        daten.push_back(n_i);
     }
     fin.close();
 }
